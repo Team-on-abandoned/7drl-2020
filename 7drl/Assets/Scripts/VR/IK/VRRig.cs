c5f8d398d7284ab3f6643df1534231e6438f8d74
Team <timonsol13@gmail.com>
2020-03-02 05:00:10 +0200

    IK for hands

---
+++ 7drl/Assets/Scripts/VR/IK/VRRig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class VRRig : MonoBehaviour {
+	[SerializeField] float turnSmooth;
+
+	[SerializeField] VRMap head;
+	[SerializeField] VRMap leftHand;
+	[SerializeField] VRMap rightHand;
+
+	[SerializeField] Transform headConstraint;
+	[SerializeField] Vector3 headBodyOffset;
+
+	void Start() {
+		headBodyOffset = transform.position - headConstraint.position;
+	}
+
+	void LateUpdate() {
+		transform.position = headConstraint.position + headBodyOffset;
+		transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized, Time.unscaledDeltaTime * turnSmooth);
+
+		head.Map();
+		leftHand.Map();
+		rightHand.Map();
+	}
+}
+
+[Serializable]
+public class VRMap {
+	public Transform vrTarget;
+	public Transform rigTarget;
+	public Vector3 trackingPosOffset;	
+	public Vector3 trackingRotOffset;	
+
+	public void Map() {
+		rigTarget.position = vrTarget.TransformPoint(trackingPosOffset);
+		rigTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotOffset);
+	}
+}
---
+++ 7drl/Assets/Thirdparty/FPSCounter.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) 2016 Unity Technologies. MIT license - license_unity.txt
+// #NVJOB FPS counter and graph - simple and fast. MIT license - license_nvjob.txt
+// #NVJOB FPS counter and graph - simple and fast V1.2 - https://nvjob.github.io/unity/nvjob-fps-counter-and-graph
+// #NVJOB Nicholas Veselov (independent developer) - https://nvjob.github.io
+
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour {
+    public Text counterText;
+    public int frameUpdate = 60;
+    public int highestPossibleFPS = 300;
+    public float graphUpdate = 1.0f;
+    public Color graphColor = new Color(1, 1, 1, 0.5f);
+
+    int curCount;
+
+    void Update() {
+        curCount = StFPS.Counter(frameUpdate, Time.deltaTime);
+        counterText.text = "FPS: " + curCount.ToString();
+    }
+}
+
+public static class StFPS {
+    static List<float> fpsBuffer = new List<float>();
+    static float fpsB;
+    static int fps;
+
+    public static int Counter(int frameUpdate, float deltaTime) {
+        int fpsBCount = fpsBuffer.Count;
+
+        if (fpsBCount <= frameUpdate) fpsBuffer.Add(1.0f / Time.deltaTime);
+        else {
+            for (int f = 0; f < fpsBCount; f++) fpsB += fpsBuffer[f];
+            fpsBuffer = new List<float> { 1.0f / Time.deltaTime };
+            fpsB = fpsB / fpsBCount;
+            fps = Mathf.RoundToInt(fpsB);
+            fpsB = 0;
+        }
+
+        return fps;
+    }
+}

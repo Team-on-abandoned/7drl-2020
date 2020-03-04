81801fe9a88bf3b22570f8d4d01ffc7a2a5747c6
Team <timonsol13@gmail.com>
2020-03-05 01:16:31 +0200

    Smooth turn

--- 7drl/Assets/Scripts/VR/Player/SmoothTurn.cs
+++ 7drl/Assets/Scripts/VR/Player/SmoothTurn.cs
@@ -1,6 +1,12 @@
 ﻿using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class SmoothTurn : ATurnType {
+    [SerializeField] float speed;
+    [SerializeField] SteamVR_Action_Vector2 input;
+
+    float rotateVal;
 
     public override void Init() {
 
@@ -12,5 +18,19 @@ public class SmoothTurn : ATurnType {
 
     public override void Use() {
         enabled = true;
+    }
+
+    void Update() {
+        if (Mathf.Abs(input.axis.x) > 0.1f) {
+            rotateVal = input.axis.x;
+        }
+        else {
+            rotateVal = 0;
+        }
+    }
+
+    private void FixedUpdate() {
+        if (rotateVal != 0)
+            transform.Rotate(Vector3.up, speed * rotateVal * Time.deltaTime);
     }
 }
